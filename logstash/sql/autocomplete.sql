b9f8b47ac0ae182eb81125ac63e636172dd15c5f
banseok1216 <bansuk1216@naver.com>
2025-11-07 16:35:03 +0900

    TORY-371 [SEARCH] 검색시 인분수 검색 색인
    refactor: 레시피 자동완성 및 상세 조회 SQL 개선
    	•	autocomplete.sql에서 servings(인분 정보)를 가중치 기반 카운트와 함께 포함하도록 업데이트
    	•	recipes.sql에서 recipe_detail_meta 테이블과 조인하여 servings 텍스트를 가져오도록 수정
    	•	색인 생성을 위해 recipes_template.json에 servings_text 필드 추가

--- logstash/sql/autocomplete.sql
+++ logstash/sql/autocomplete.sql
@@ -1,37 +1,52 @@
 SELECT
     all_text.text,
-    COUNT(*) AS count
+    CAST(ROUND(SUM(all_text.weight)) AS UNSIGNED) AS count
 FROM (
     SELECT
-    yi.title AS text
+        yi.title AS text,
+        1.0 AS weight
     FROM
-    recipe r
+        recipe r
     LEFT JOIN
-    recipe_youtube_meta yi ON r.id = yi.recipe_id
+        recipe_youtube_meta yi ON r.id = yi.recipe_id
     WHERE
-    r.recipe_status = 'SUCCESS' AND yi.title IS NOT NULL
+        r.recipe_status = 'SUCCESS' AND yi.title IS NOT NULL
 
     UNION ALL
 
     SELECT
-    i.name AS text
+        i.name AS text,
+        1.0 AS weight
     FROM
-    recipe_ingredient i
+        recipe_ingredient i
     JOIN
-    recipe r ON i.recipe_id = r.id
+        recipe r ON i.recipe_id = r.id
     WHERE
-    r.recipe_status = 'SUCCESS'
+        r.recipe_status = 'SUCCESS'
 
     UNION ALL
 
     SELECT
-    t.tag AS text
+        t.tag AS text,
+        1.0 AS weight
     FROM
-    recipe_tag t
+        recipe_tag t
     JOIN
-    recipe r ON t.recipe_id = r.id
+        recipe r ON t.recipe_id = r.id
     WHERE
-    r.recipe_status = 'SUCCESS'
-    ) AS all_text
-GROUP BY
-    all_text.text;
+        r.recipe_status = 'SUCCESS'
+    
+    UNION ALL
+    
+    SELECT
+        CONCAT(rdm.servings, '인분') AS text,
+        0.1 AS weight
+    FROM
+        recipe_detail_meta rdm
+    JOIN
+        recipe r ON rdm.recipe_id = r.id
+    WHERE
+        r.recipe_status = 'SUCCESS'
+        AND rdm.servings IS NOT NULL
+) AS all_text
+GROUP BY all_text.text;
